c9888ec1d678e6bcfbe656f7adce75799e535580
bennett2winit <blakbenn@gmail.com>
2023-08-27 15:55:13 -0500

    Added the function to replace the values in the arrays with each players marker. I also added an array with empty spaces to highlight the players places. The numbered array will likely be an option in the future to assist in placement, but not the typical board that is printed.

--- ttti.py
+++ ttti.py
@@ -1,21 +1,32 @@
-# Here we define the board that will be printed each time the "board" function is called
+# Here we define the board that will be printed each time the "board" function is called. 
+# Additionally there is a board with numbered spaces to visualize the spaces.
 spaces = ['1','2','3','4','5','6','7','8','9']
+emptySpaces = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
-def board():
+# This is the function that prints the board. It references the index of spaces to print out the value of the board.
+def board(array):
+    print("\n")
     for i in range(3):
-        print(f"| {spaces[3*i]} | {spaces[3*i + 1]} | {spaces[3*i + 2]} |")
+        print(f"  {array[3*i]} | {array[3*i + 1]} | {array[3*i + 2]}  ")
         if i == 2:
             print("\n")
         else:
             print("-" * 13)
 
+# Here we define the function to replace the value in spaces with an X or an O.
+def play(marker, space):
+    spaces[int(space) - 1] = marker 
+    emptySpaces[int(space) - 1] = marker
+
 # Now we define what all occurs during a turn.
 def turn(player):
    if player == 1:
-        marker = ' X '
+       marker = 'X'
    elif player == 2:
-       marker = ' O '
-   print(f"Player {player} where would you like to place your {marker}?")
-
-board()
+       marker = 'O'
+   space = input(f"Player {player} where would you like to place your {marker}?\n")
+   play(marker, space)
+   
+board(spaces)
 turn(2)
+board(emptySpaces)
